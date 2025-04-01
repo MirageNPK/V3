7caf30c97a9e0be4c73856478bf4038c27148045
MirageNPK <p.pryimak@netpeak.group>
2025-04-01 17:06:05 +0300

    q

--- sync_project/sync_project/prod_settings.py
+++ sync_project/sync_project/prod_settings.py
@@ -35,3 +35,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Директорія дл
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Якщо у вас є власні статичні файли
 ]
+STATIC_URL = '/static/'
